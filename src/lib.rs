8c192f75a3fec73643f9e8dddf65d619e3df48f4
Valerii Praid <valerii.praid@gmail.com>
2019-11-17 13:31:23 +0900

    Add IntoResult trait

--- src/lib.rs
+++ src/lib.rs
@@ -19,4 +19,24 @@ pub trait IntoOption where Self: Sized {
     }
 }
 
+pub trait IntoResult where Self: Sized {
+    fn ok_if<P, E>(self, predicate: P, error: E) -> Result<Self, E>
+    where P: Fn(&Self) -> bool {
+        if predicate(&self) {
+            Ok(self)
+        } else {
+            Err(error)
+        }
+    }
+    fn err_if<P, E>(self, predicate: P, error: E) -> Result<Self, E>
+    where P: Fn(&Self) -> bool {
+        if predicate(&self) {
+            Err(error)
+        } else {
+            Ok(self)
+        }
+    }
+}
+
 impl<T> IntoOption for T {}
+impl<T> IntoResult for T {}
