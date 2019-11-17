9d7a3a7142ab05ee9edf2a8285dbb4de97cf1125
Valerii Praid <valerii.praid@gmail.com>
2019-11-17 13:11:52 +0900

    Add tests from String::is_empty

--- tests/lib.rs
+++ tests/lib.rs
@@ -9,3 +9,17 @@ fn none_on_zero() {
 fn some_on_zero() {
     assert_eq!(Some(0), 0.some_if(|x| *x == 0))
 }
+
+#[test]
+fn some_on_empty_string() {
+    assert_eq!(
+        Some("".to_string()),
+        "".to_string().some_if(String::is_empty))
+}
+
+#[test]
+fn none_on_empty_string() {
+    assert_eq!(
+        None,
+        "".to_string().none_if(String::is_empty))
+}
