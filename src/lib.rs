df0de576fb27bc3c3b1e09e1da96afca527e97b8
Valerii Praid <valerii.praid@gmail.com>
2019-11-17 13:52:51 +0900

    Add top-level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+//! This library provides two convenient traits that allow you to convert values into option or result
+//! based on the provided predicate. It is somewhat similar to the boolinator crate, except you don't
+//! need to create a boolean - the predicate will do it for you. This can be useful e.g. when writing a
+//! long chain that endsd with a fold which you want to convert into Option or Result.
+//!
+//! ```
+//! use intoif::IntoOption;
+//! fn fizz_buzz(n: u32) -> String {
+//!     [(3, "Fizz"), (5, "Buzz")]
+//!         .iter()
+//!         .filter_map(|(x, s)| if n % x == 0 { Some(*s) } else { None })
+//!         .collect::<String>()
+//!         .none_if(String::is_empty)
+//!         .unwrap_or_else(|| n.to_string())
+//! }
+//! ```
+
 #![no_std]
 
 pub trait IntoOption where Self: Sized {
