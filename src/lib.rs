93ddcb96c5eaaf6b17624b4f0658f78bc1864b73
Valerii Praid <valerii.praid@gmail.com>
2019-11-17 13:57:24 +0900

    Add documentation to traits and methods

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,9 @@
 
 #![no_std]
 
+/// Allow construction of Option from any type using predicate to choose between Some and None.
 pub trait IntoOption where Self: Sized {
+    /// Returns Some(self) if predicate returns true on self, None otherwise.
     fn some_if<P>(self, predicate: P) -> Option<Self>
     where P: Fn(&Self) -> bool {
         if predicate(&self) {
@@ -26,6 +28,7 @@ pub trait IntoOption where Self: Sized {
             None
         }
     }
+    /// Returns None if predicate returns true on self, Some(self) otherwise.
     fn none_if<P>(self, predicate: P) -> Option<Self>
     where P: Fn(&Self) -> bool {
         if predicate(&self) {
@@ -36,7 +39,9 @@ pub trait IntoOption where Self: Sized {
     }
 }
 
+/// Allow construction of Result from any type using predicate to choose between Ok and Err.
 pub trait IntoResult where Self: Sized {
+    /// Returns Ok(self) if predicate returns true on self, Err(error) otherwise.
     fn ok_if<P, E>(self, predicate: P, error: E) -> Result<Self, E>
     where P: Fn(&Self) -> bool {
         if predicate(&self) {
@@ -45,6 +50,7 @@ pub trait IntoResult where Self: Sized {
             Err(error)
         }
     }
+    /// Returns Err(error) if predicate returns true on self, Ok(self) otherwise.
     fn err_if<P, E>(self, predicate: P, error: E) -> Result<Self, E>
     where P: Fn(&Self) -> bool {
         if predicate(&self) {
