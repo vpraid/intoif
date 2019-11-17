8faccfbff5261f8fee5e943307fd9240f815e9ce
Valerii Praid <valerii.praid@gmail.com>
2019-11-17 13:31:35 +0900

    Add tests for IntoResult

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,13 +1,14 @@
 use into_option::IntoOption;
-
-#[test]
-fn none_on_zero() {
-    assert_eq!(None, 0.none_if(|x| *x == 0))
-}
+use into_option::IntoResult;
 
 #[test]
 fn some_on_zero() {
     assert_eq!(Some(0), 0.some_if(|x| *x == 0))
+}
+
+#[test]
+fn none_on_zero() {
+    assert_eq!(None, 0.none_if(|x| *x == 0))
 }
 
 #[test]
@@ -22,4 +23,28 @@ fn none_on_empty_string() {
     assert_eq!(
         None,
         "".to_string().none_if(String::is_empty))
+}
+
+#[test]
+fn ok_on_zero() {
+    assert_eq!(Ok(0), 0.ok_if(|x| *x == 0, "omae"))
+}
+
+#[test]
+fn err_on_zero() {
+    assert_eq!(Err("wa"), 0.err_if(|x| *x == 0, "wa"))
+}
+
+#[test]
+fn ok_on_empty_string() {
+    assert_eq!(
+        Ok("".to_string()),
+        "".to_string().ok_if(String::is_empty, "mou"))
+}
+
+#[test]
+fn err_on_empty_string() {
+    assert_eq!(
+        Err("shindeiru"),
+        "".to_string().err_if(String::is_empty, "shindeiru"))
 }
